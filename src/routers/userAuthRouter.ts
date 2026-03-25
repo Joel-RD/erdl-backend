66aa336e1375663b0d3d3cefd119b5998c4e8153
Joel-RD <eudy.joel.reyes@gmail.com>
2026-03-25 15:03:51 -0400

    chore: disable authentication routes.

--- src/routers/userAuthRouter.ts
+++ src/routers/userAuthRouter.ts
@@ -9,8 +9,8 @@ const router = Router();
 const userAuthRepositorys = new AuthRepository(turso);
 const userAuthControllers = new userAuthController(userAuthRepositorys);
 
-router.post("/auth/verify-email", limitAuthButton, verifySendToEmail, userAuthControllers.postAuthVerifyEmailController);
-router.post("/auth/register", limitAuthButton, userAuthControllers.authRegisterController);
-router.post("/auth/login", limitAuthButton, userAuthControllers.authLoginController);
+// router.post("/auth/verify-email", limitAuthButton, verifySendToEmail, userAuthControllers.postAuthVerifyEmailController);
+// router.post("/auth/register", limitAuthButton, userAuthControllers.authRegisterController);
+// router.post("/auth/login", limitAuthButton, userAuthControllers.authLoginController);
 
 export default router;
